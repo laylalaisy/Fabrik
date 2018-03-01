0889d76bb3f6bbdccaa5adbbdf89f500e2603c86
laylalaisy <lsylayla@163.com>
2018-02-28 21:36:45 -0800

    Add searchbar and title(#311)

--- ide/static/js/modelZoo.js
+++ ide/static/js/modelZoo.js
@@ -2,77 +2,198 @@ import React from 'react';
 import ModelElement from './modelElement';
 
 class ModelZoo extends React.Component {
+
+  constructor() {
+      super();
+      this.state = {
+        open:0,
+        text:"Load More",
+        Recognition:{ 
+                      toDisplay:6,
+                      models:[
+                              ["keras","v3","Inception V3"],
+                              ["caffe","GoogleNet","GoogLeNet"],
+                              ["caffe","alexnet","AlexNet"],
+                              ["caffe","vgg16","VGG 16"],
+                              ["caffe","densenet","DenseNet"],
+                              ["caffe","lenet","MNIST LeNet"],
+                              ["caffe","cifar10_full","Cifar10 CNN"],
+                              ["keras","imdb_cnn_lstm","IMDB CNN LSTM"],
+                              ["caffe","All_CNN","All CNN"],
+                              ["caffe","Caffenet","Caffenet"],
+                              ["caffe","speech_net","Speech Net"],
+                              ["caffe","CNDS","CNDS"],
+                              ["caffe","Gender","Gender"],
+                              ["caffe","Age","Age"],
+                              ["caffe","network_in_network","Network in Network"],
+                              ["caffe","resnet101","ResNet 101"],
+                              ["caffe","Squeezenet","Squeezenet"],
+                              ["caffe","SENet","SENet"]
+                            ] 
+                  },
+        Detection:{ 
+                    toDisplay:3,
+                    models: [
+                              ["caffe","yolo_net","YOLONet"],
+                              ["caffe","vanilla","Vanilla CNN"],
+                              ["caffe","R-CNN","R-CNN"],
+                              ["caffe","fcn","FCN32 Pascal"],
+                              ["caffe","48Net","48Net"]
+                            ]
+                  },
+        Seq2Seq:{
+                  toDisplay:3,
+                  models:[
+                          ["keras","textGeneration","Text Generation"],
+                          ["keras","seq2seq_lang","Seq2Seq Translation"],
+                          ["caffe","pix2pix","Pix2Pix"]
+                        ]
+                },
+        VQA    :{
+                  toDisplay:3,
+                  models:[
+                            ["keras","VQA","VQA"],
+                            ["keras","VQA2","VQA2"],
+                            ["caffe","mlpVQA","VQS"]
+                          ]
+                },
+        Segmentation:{
+                    toDisplay:2,
+                    models:[
+                              ["caffe","fcn2","Semantic Segmentation"],
+                              ["keras","ZF_UNET_224","UNET"]
+                            ]
+                }
+      };
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleClick(event){
+    if(this.state.open){
+      this.setState({text:"Load More"});
+    }
+    else{
+      this.setState({text:"Load Less"}); 
+    }
+    if (event.target.id =="change")
+      this.setState({open:1-this.state.open});
+  }
+
+  handlerSearch(){
+    let bar = React.findDOMNode(this.refs.searchBar);
+    let word = bar.value;
+    this.props.searchInvItem(word);
+  }
+
   render() {
+    const startIndex = 0;
+    var category = this.state.Recognition;
+    var finalIndex = category.toDisplay*(1 - this.state.open)+this.state.open*category.models.length;
+    var sliced = category.models.slice(startIndex, finalIndex);
+    const renderRecognition = sliced.map((category) => {
+      return (  
+        <div>
+          <ModelElement importNet = {this.props.importNet} framework =
+                      {category[0]} id = {category[1]}> {category[2]} </ModelElement>
+          <br/>
+        </div>
+        );
+    });
+
+
+    category = this.state.Detection;
+    finalIndex = category.toDisplay*(1 - this.state.open)+this.state.open*category.models.length;
+    sliced = category.models.slice(startIndex, finalIndex);
+    const renderDetection = sliced.map((category) => {
+      return (  
+        <div>
+          <ModelElement importNet = {this.props.importNet} framework =
+                      {category[0]} id = {category[1]}> {category[2]} </ModelElement>
+          <br/>
+        </div>
+        );
+    });
+
+    category = this.state.Seq2Seq;
+    finalIndex = category.toDisplay*(1 - this.state.open)+this.state.open*category.models.length;
+    sliced = category.models.slice(startIndex, finalIndex);
+    const renderSeq2Seq = sliced.map((category) => {
+      return (  
+        <div>
+          <ModelElement importNet = {this.props.importNet} framework =
+                      {category[0]} id = {category[1]}> {category[2]} </ModelElement>
+          <br/>
+        </div>
+        );
+    });
+
+    category = this.state.Segmentation;
+    finalIndex = category.toDisplay*(1 - this.state.open)+this.state.open*category.models.length;
+    sliced = category.models.slice(startIndex, finalIndex);
+    const renderSegmentation = sliced.map((category) => {
+      return (  
+        <div>
+          <ModelElement importNet = {this.props.importNet} framework =
+                      {category[0]} id = {category[1]}> {category[2]} </ModelElement>
+          <br/>
+        </div>
+        );
+    });
+
+    category = this.state.VQA;
+    finalIndex = category.toDisplay*(1 - this.state.open)+this.state.open*category.models.length;
+    sliced = category.models.slice(startIndex, finalIndex);
+    const renderVQA = sliced.map((category) => {
+      return (  
+        <div>
+          <ModelElement importNet = {this.props.importNet} framework =
+                      {category[0]} id = {category[1]}> {category[2]} </ModelElement>
+          <br/>
+        </div>
+        );
+    });
+
+
     return (
       <div className="zoo-modal">
         <div className="centered-zoo-modal">
           <div className="zoo-modal-model">
+            <h2 className="zoo-modal-text">Load From Zoo</h2>
+            <input className="import-textbox-input" ref='searchBar' onChange={this.handlerSearch.bind(this)} type="text" placeholder="Search..." />
+          </div>
+          <div className="zoo-modal-model">
             <h3 className="zoo-modal-text">Recognition</h3>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="lenet">MNIST LeNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="cifar10_full">Cifar10 CNN</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="alexnet">AlexNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="All_CNN">All CNN</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="vgg16">VGG 16</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="densenet">DenseNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="GoogleNet">GoogLeNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="resnet101">ResNet 101</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="v3">Inception V3</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="Squeezenet">Squeezenet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="SENet">SENet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="imdb_cnn_lstm">IMDB CNN LSTM</ModelElement>
+            {renderRecognition}
           </div>
           <div className="zoo-modal-model">
             <h3 className="zoo-modal-text">Detection</h3>
-            <ModelElement importNet={this.props.importNet} framework="caffe" id="vanilla">Vanilla CNN</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="fcn">FCN32 Pascal</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="yolo_net">YOLONet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="HED">HED</ModelElement>
+              {renderDetection}
             <h3 className="zoo-modal-text">Retrieval</h3>
               <ModelElement importNet={this.props.importNet} framework="caffe" id="siamese_mnist">MNIST Siamese</ModelElement>
           </div>
           <div className="zoo-modal-model">
             <h3 className="zoo-modal-text">Seq2Seq</h3>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="textGeneration">Text Generation</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="seq2seq_lang">Seq2Seq Translation</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="pix2pix">Pix2Pix</ModelElement>
+              {renderSeq2Seq}
           </div>
           <div className="zoo-modal-model">
             <h3 className="zoo-modal-text">Caption</h3>
             <ModelElement importNet={this.props.importNet} framework="caffe" id="CoCo_Caption">CoCo Caption</ModelElement>
             <h3 className="zoo-modal-text">Segmentation</h3>
-            <ModelElement importNet={this.props.importNet} framework="caffe" id="fcn2">Semantic Segmentation</ModelElement>
-            <br/>
-            <ModelElement importNet={this.props.importNet} framework="keras" id="ZF_UNET_224">UNET</ModelElement>
-          </div>
-          <div className="zoo-modal-model">
+                {renderSegmentation}
             <h3 className="zoo-modal-text">VQA</h3>
-            <ModelElement importNet={this.props.importNet} framework="keras" id="VQA">VQA</ModelElement>
-            <br/>
-            <ModelElement importNet={this.props.importNet} framework="keras" id="VQA2">VQA2</ModelElement>
-            <br/>
-            <ModelElement importNet={this.props.importNet} framework="caffe" id="mlpVQA">VQS</ModelElement>
+              {renderVQA}
           </div>
+          <button className="import-textbox-button btn btn-default col-md-2 col-md-offset-5"  
+              id="change" onClick={this.handleClick}>{this.state.text} 
+            &nbsp;<span className="glyphicon  glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+          </button>   
         </div>
       </div>
     );
   }
 }
+
 
 ModelZoo.propTypes = {
   importNet: React.PropTypes.func
